67ef176f95f7e7b0acb4c3c2fdec1288a4d5bf44
Kyle Robinson Young <kyle@dontkry.com>
2014-12-17 10:39:33 -0800

    Ability to run individual tests

--- test/all.js
+++ test/all.js
@@ -1,3 +1,16 @@
-require('./multi');
-require('./events');
-require('./attrib');
+var path = require('path');
+var tests = [
+  'multi',
+  'events',
+  'attrib',
+];
+
+var limit = process.argv[2];
+
+if (limit) {
+  require(path.resolve(process.cwd(), limit));
+} else {
+  tests.forEach(function(name) {
+    require(path.resolve(__dirname, name + '.js'));
+  });
+}
